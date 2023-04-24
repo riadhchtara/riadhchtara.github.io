1325cdd5bf21f127cb8b0287785625ecf597595b
riadhchtara <riadh.chtara@gmail.com>
2023-04-24 16:34:01 +0100

    add buttons

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 
 $(document).ready(function () {
     console.log("riadh");
+    $( ".widget input[type=submit], .widget a, .widget button" ).button();
+    $( "button, input, a" ).on( "click", function( event ) {
+      event.preventDefault();
+    } );
 }); 
