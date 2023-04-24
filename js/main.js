a9305dfb466290e8ef524ca0bd0d06f5c31c2963
riadhchtara <riadh.chtara@gmail.com>
2023-04-24 17:28:36 +0100

    ADD dialog 2

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 
 $(document).ready(function () {
   console.log("riadh");
-  $( "#dialog" ).dialog();
+  $("#dialog0").dialog();
+  $("#dialog1").dialog();
   $(".widget input[type=submit], .widget a, .widget button").button();
   $("button, input, a").on("click", function (event) {
     event.preventDefault();
