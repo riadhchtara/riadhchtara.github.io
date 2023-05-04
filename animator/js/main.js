ccc8e523b44e28c39259e6c0d08cba136782241d
riadhchtara <riadh.chtara@gmail.com>
2023-05-04 12:34:41 +0100

    comment dialog

--- animator/js/main.js
+++ animator/js/main.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
   $("#actions").resizable();
   $("#actions").width(200);
   console.log("riadh");
-  $("#dialog0").dialog();
+  //$("#dialog0").dialog();
   $("#dialog1").dialog();
   $(".widget input[type=submit], .widget a, .widget button").button();
   $("button, input, a").on("click", function (event) {
